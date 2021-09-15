fefeaa721f91cf4e44e25f308bfd490059b86cc1
javier.menga <jfm94@hotmail.com>
2021-09-15 20:46:29 -0300

    generando eventos

--- script/usuario.js
+++ script/usuario.js
@@ -8,41 +8,46 @@ class Clientes {
         this.alimentacion = alimentacion;
     }
 }
-let usuarioActivo;
-let resp = confirm(`Bienvenido, ¿Ya estás registrado?`);
-if (resp) {
-    ingresar();
-} else {
-    let resp = confirm(`¿Querés registrarte?`);
-    if (resp) {
-        registrar();
-    }
-}
 
-// Activar el usuario ingresado/registrado
-let contenedor_usuario = document.getElementById("userIcon");
-let usuario_activo = document.createElement("p")
-usuario_activo.textContent = usuarioActivo.nombre;
-contenedor_usuario.appendChild(usuario_activo);
-usuario_activo.classList.add("hola");
+let usuarioActivo;
+
+// Ingresar al clickear el botón INGRESAR
+let login = document.getElementById("login");
+login.addEventListener("click", ingresar);
+let clave_input = document.getElementById("password");
+clave_input.addEventListener("keydown", enter)
+
+// Enviar a la página de registro al clickear el botón REGISTRAR
+let register = document.getElementById("register");
+register.addEventListener("mousedown", habilitar_inputs);
+register.addEventListener("mousedown", inhabilitar_ingreso);
+register.addEventListener("click", registrar);
+let mail_input = document.getElementById("email");
+mail_input.addEventListener("keydown", enter)
 
 
 // FUNCIONES DEFINIDAS
 function ingresar() {
-    let usuario = prompt("Ingresá tu usuario");
-    let clave = prompt("Ingresá tu contraseña");
+    let usuario = document.getElementById("user").value;
+    let clave = document.getElementById("password").value;
     usuarioActivo = buscar_usuario(usuario);
     let clave2 = validar_clave(clave, usuario);
     if (usuarioActivo && clave2) {
-        alert(`Bienvenido ${usuarioActivo.nombre} ${usuarioActivo.apellido}`);
+        plasmar_usuario();
     } else if (!usuarioActivo) {
-        let esc = confirm("El usuario que ingresaste no existe. Querés volver a intentarlo?");
-        if (esc) {
-            ingresar();
-        }
+        alert("El usuario que ingresaste no existe");
     } else if (!clave2) {
         alert("Ingresá una contraseña correcta");
+    }
+    return usuarioActivo;
+}
+
+function enter(event) {
+    tecla_enter = event.keyCode;
+    if (event.currentTarget = clave_input && tecla_enter == 13) {
         ingresar();
+    } else if (event.currentTarget = mail_input && tecla_enter == 13) {
+        registrar();
     }
 }
 
@@ -56,7 +61,6 @@ function buscar_usuario(usuario_ingresado) {
         while (!encontrado && i != usuarios_almacenados.length) {
             if (usuarios_almacenados[i].usuario == usuario_ingresado) {
                 encontrado = usuarios_almacenados[i];
-                console.log(encontrado);
                 return encontrado;
             }
             i++;
@@ -77,19 +81,27 @@ function validar_clave(clave_ingresada, usuario_ingresado) {
     return comparacion;
 }
 
+function habilitar_inputs() {
+    let hidden = document.getElementsByClassName("hidden");
+    for (const x of hidden) {
+        x.classList.replace("hidden", "visible");
+    }
+}
+
 function registrar() {
 
-    let usuario = prompt("Ingresá un usuario");
-    let nombre = prompt("Ingresá tu nombre").toLowerCase();
-    let apellido = prompt("Ingresá tu apellido").toLowerCase();
-    let clave = prompt("Ingresá tu contraseña");
-    let edad = prompt("Ingresá tu edad");
-    let alimentacion = prompt("Ingresá tu tipo de alimentación. Opciones: carnívora/vegetariana/vegana/celíaca/mixta").toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "");
+    let usuario = document.getElementById("user").value;
+    let nombre = document.getElementById("name").value.toLowerCase();
+    let apellido = document.getElementById("lastName").value.toLowerCase();
+    let clave = document.getElementById("password").value;
+    let edad = document.getElementById("age").value;
+    let alimentacion = document.getElementById("feeding_type").value;
     let mjs = chequear_dato(usuario, nombre, apellido, clave, edad, alimentacion);
     if (mjs == ``) {
         usuarioActivo = new Clientes(usuario, nombre, apellido, clave, parseInt(edad), alimentacion)
         guardar_usuario(usuarioActivo);
         alert(`Usuario creado con éxito!`);
+        plasmar_usuario();
     } else {
         alert(mjs);
     }
@@ -107,8 +119,6 @@ function chequear_dato(usuario, nombre, apellido, clave, edad, alimentacion) {
 
             // } else if (alimentacion != "carnivora" || alimentacion != "vegetariana" || alimentacion != "vegana" || alimentacion != "celiaca" || alimentacion != "mixta") {
             //     mjs = "No ingresaste un tipo de alimentación válido";
-        } else {
-            alert(mjs);
         }
 
     } else {
@@ -129,4 +139,17 @@ function guardar_usuario(nuevo_usuario_ingresado) {
         let usuarios_almacenados_string = JSON.stringify(usuarios_almacenados);
         localStorage.setItem("lista_usuarios", usuarios_almacenados_string);
     }
+}
+
+function plasmar_usuario() {
+    // Visualizar en HTML el usuario ingresado/registrado
+    let contenedor_usuario = document.getElementById("userIcon");
+    let usuario_activo = document.createElement("p");
+    usuario_activo.textContent = usuarioActivo.nombre;
+    contenedor_usuario.appendChild(usuario_activo);
+    usuario_activo.classList.add("active_user");
+}
+
+function inhabilitar_ingreso() {
+    login.classList.add("hidden");
 }
