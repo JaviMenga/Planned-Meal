1612f97ba448db254b2e904a34b7ec6a1de7eb5a
javier.menga <jfm94@hotmail.com>
2021-09-27 18:44:59 -0300

    .

--- script/functions.js
+++ script/functions.js
@@ -1,11 +1,13 @@
 // FUNCIONES DEFINIDAS
 
 // B
+
+// puedo sustituir el While por un for! HACER
 function buscar_usuario(usuario_ingresado) {
-    if (!localStorage.getItem("lista_usuarios")) {
+    if (!localStorage.getItem(`lista_usuarios`)) {
         return false
     } else {
-        let usuarios_almacenados = JSON.parse(localStorage.getItem("lista_usuarios"));
+        let usuarios_almacenados = JSON.parse(localStorage.getItem(`lista_usuarios`));
         let i = 0
         let encontrado = false;
         while (!encontrado && i != usuarios_almacenados.length) {
@@ -20,107 +22,39 @@ function buscar_usuario(usuario_ingresado) {
 }
 
 // C
-function creando_cartas(x, i) {
-    let contenedor_cartas = document.getElementById(`contenedor-cartas`);
-    let contenedor_carta = document.createElement(`div`);
-    contenedor_carta.classList.add(`card-header`, `m-2`, `card--style`)
-    contenedor_cartas.appendChild(contenedor_carta);
+// Esta funcion está hecha con JQUERY
+function creando_cartas(receta, i) {
+    let contenedor_cartas = $(`#contenedor-cartas`);
+    contenedor_cartas.append(`<div class="card-header m-2 card--style">
+                                <img class="card-img-top card__img--size" src="${receta.imagen}" alt="Foto de ${receta.nombre}">
+                                <div class="card-body">
+                                    <h5 class="card-title card-title--style">${receta.nombre}</h5>
+                                    <div class="card-body d-flex flex-column">
+                                        <button class="btn btn-color m-1" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal${i}">Ver receta</button>
+                                        <div class="modal fade" id="exampleModal${i}" tabindex="-1" aria-labellebdy="exampleModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog modal-xl">
+                                                <div class="modal-content modal-content--color">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title card-title--style" id="exampleModalLabel">${receta.nombre}</h5>
+                                                        <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                    </div>
+                                                    <div class="modal-body text-start">
+                                                        <ul>
+                                                            <li>${receta.instrucciones}</li>
+                                                        </ul>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button class="btn btn-color m-1" type="button" data-bs-dismiss="modal">Volver</button>
+                                                        <button class="btn btn-color m-1" type="button">Agregar</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <a class="btn btn-color a-style m-1">Agregar</a>
+                                    </div>
+                                </div>
+                            </div>`);
 
-    let imagen_carta = document.createElement(`img`);
-    imagen_carta.classList.add(`card-img-top`, `card__img--size`);
-    imagen_carta.setAttribute(`src`, x.imagen);
-    imagen_carta.setAttribute(`alt`, `Foto de ${x.nombre}`);
-    contenedor_carta.appendChild(imagen_carta);
-
-    let cuerpo_carta = document.createElement(`div`);
-    cuerpo_carta.classList.add(`card-body`);
-    contenedor_carta.appendChild(cuerpo_carta);
-
-    let nombre_carta = document.createElement(`h5`);
-    nombre_carta.classList.add(`card-title`, `card-title--style`);
-    nombre_carta.textContent = x.nombre;
-    cuerpo_carta.appendChild(nombre_carta);
-
-    let contenedor_botones_carta = document.createElement(`div`);
-    contenedor_botones_carta.classList.add(`card-body`, `d-flex`, `flex-column`);
-    cuerpo_carta.appendChild(contenedor_botones_carta);
-
-    let botones_carta = document.createElement(`button`);
-    botones_carta.classList.add(`btn`, `btn-color`, `m-1`);
-    botones_carta.setAttribute(`type`, `button`);
-    botones_carta.setAttribute(`data-bs-toggle`, `modal`);
-    botones_carta.setAttribute(`data-bs-target`, `#exampleModal${i}`);
-    botones_carta.textContent = `Ver receta`;
-    contenedor_botones_carta.appendChild(botones_carta);
-    console.log(botones_carta);
-
-    let contenedor_instrucciones_carta = document.createElement(`div`);
-    contenedor_instrucciones_carta.classList.add(`modal`, `fade`);
-    contenedor_instrucciones_carta.setAttribute(`id`, `exampleModal${i}`);
-    contenedor_instrucciones_carta.setAttribute(`tabindex`, `-1`);
-    contenedor_instrucciones_carta.setAttribute(`aria-labelledby`, `exampleModalLabel`);
-    contenedor_instrucciones_carta.setAttribute(`aria-hidden`, `true`);
-    contenedor_botones_carta.appendChild(contenedor_instrucciones_carta);
-
-    let div1_contenedor_instrucciones_carta = document.createElement(`div`);
-    div1_contenedor_instrucciones_carta.classList.add(`modal-dialog`, `modal-dialog`, `modal-xl`);
-    contenedor_instrucciones_carta.appendChild(div1_contenedor_instrucciones_carta);
-
-    let div2_contenedor_instrucciones_carta = document.createElement(`div`);
-    div2_contenedor_instrucciones_carta.classList.add(`modal-content`, `modal-content--color`);
-    div1_contenedor_instrucciones_carta.appendChild(div2_contenedor_instrucciones_carta);
-
-    let div3_contenedor_instrucciones_carta = document.createElement(`div`);
-    div3_contenedor_instrucciones_carta.classList.add(`modal-header`);
-    div2_contenedor_instrucciones_carta.appendChild(div3_contenedor_instrucciones_carta);
-
-    let titulo_intrucciones_carta = document.createElement(`h5`);
-    titulo_intrucciones_carta.classList.add(`modal-title`, `card-title--style`);
-    titulo_intrucciones_carta.setAttribute(`id`, `exampleModalLabel`);
-    titulo_intrucciones_carta.textContent = x.nombre;
-    div3_contenedor_instrucciones_carta.appendChild(titulo_intrucciones_carta);
-
-    let boton_instrucciones_carta = document.createElement(`button`);
-    boton_instrucciones_carta.classList.add(`btn-close`);
-    boton_instrucciones_carta.setAttribute(`type`, `button`);
-    boton_instrucciones_carta.setAttribute(`data-bs-dismiss`, `modal`);
-    boton_instrucciones_carta.setAttribute(`aria-label`, `Close`);
-    div3_contenedor_instrucciones_carta.appendChild(boton_instrucciones_carta);
-
-    let div4_contenedor_instrucciones_carta = document.createElement(`div`);
-    div4_contenedor_instrucciones_carta.classList.add(`modal-body`, `text-start`);
-    div2_contenedor_instrucciones_carta.appendChild(div4_contenedor_instrucciones_carta);
-
-    // Acá irian las instrucciones
-    let lista_instrucciones_carta = document.createElement(`ul`);
-    div4_contenedor_instrucciones_carta.appendChild(lista_instrucciones_carta)
-
-    let instrucciones_carta = document.createElement(`li`);
-    instrucciones_carta.textContent = x.instrucciones;
-    div4_contenedor_instrucciones_carta.appendChild(instrucciones_carta);
-
-    let link_carta = document.createElement(`a`);
-    link_carta.classList.add(`btn`, `btn-color`, `a-style`, `m-1`)
-    link_carta.setAttribute(`href`, `#`)
-    link_carta.textContent = "Agregar";
-    contenedor_botones_carta.appendChild(link_carta);
-
-    let div5_contenedor_instrucciones_carta = document.createElement(`div`);
-    div5_contenedor_instrucciones_carta.classList.add(`modal-footer`);
-    div2_contenedor_instrucciones_carta.appendChild(div5_contenedor_instrucciones_carta);
-
-    let boton1_contenedor_carta = document.createElement(`button`);
-    boton1_contenedor_carta.classList.add(`btn`, `btn-color`, `m-1`);
-    boton1_contenedor_carta.setAttribute(`type`, `button`);
-    boton1_contenedor_carta.setAttribute(`data-bs-dismiss`, `modal`);
-    boton1_contenedor_carta.textContent = `Volver`;
-    div5_contenedor_instrucciones_carta.appendChild(boton1_contenedor_carta);
-
-    let boton2_contenedor_carta = document.createElement(`button`);
-    boton2_contenedor_carta.classList.add(`btn`, `btn-color`, `m-1`);
-    boton2_contenedor_carta.setAttribute(`type`, `button`);
-    boton2_contenedor_carta.textContent = `Agregar`;
-    div5_contenedor_instrucciones_carta.appendChild(boton2_contenedor_carta);
 }
 
 function chequear_dato(usuario, nombre, apellido, clave, edad, alimentacion, mail) {
@@ -141,30 +75,31 @@ function enter(event) {
     let tecla_enter = event.keyCode;
     if (event.target == clave_input && tecla_enter == 13) {
         ingresar();
-        // esto no está funcionando
+        console.log(`entre`)
     } else if (event.target == mail_input && tecla_enter == 13) {
         registrar();
+        console.log(`entre por aquí`)
     }
 }
 
 // G
 function guardar_usuario(nuevo_usuario_ingresado) {
-    if (localStorage.getItem("lista_usuarios")) {
-        let usuarios_almacenados = JSON.parse(localStorage.getItem("lista_usuarios"));
+    if (localStorage.getItem(`lista_usuarios`)) {
+        let usuarios_almacenados = JSON.parse(localStorage.getItem(`lista_usuarios`));
         usuarios_almacenados.push(nuevo_usuario_ingresado);
         let usuarios_almacenados_string = JSON.stringify(usuarios_almacenados);
-        localStorage.setItem("lista_usuarios", usuarios_almacenados_string);
+        localStorage.setItem(`lista_usuarios`, usuarios_almacenados_string);
     } else {
         usuarios_almacenados = new Array();
         usuarios_almacenados.push(nuevo_usuario_ingresado);
         let usuarios_almacenados_string = JSON.stringify(usuarios_almacenados);
-        localStorage.setItem("lista_usuarios", usuarios_almacenados_string);
+        localStorage.setItem(`lista_usuarios`, usuarios_almacenados_string);
     }
 }
 
 // H
 function habilitar_inputs() {
-    let hidden = document.getElementsByClassName("hidden");
+    let hidden = document.getElementsByClassName(`hidden`);
     for (const x of hidden) {
         x.classList.replace("hidden", "visible");
     }
@@ -172,8 +107,8 @@ function habilitar_inputs() {
 
 // I
 function ingresar() {
-    let usuario = document.getElementById("user").value;
-    let clave = document.getElementById("password").value;
+    let usuario = document.getElementById(`user`).value;
+    let clave = document.getElementById(`password`).value;
     usuarioActivo = buscar_usuario(usuario);
     let clave2 = validar_clave(clave, usuario);
     if (usuarioActivo && clave2) {
@@ -193,8 +128,8 @@ function inhabilitar_ingreso() {
 // P
 function plasmar_usuario() {
     // Visualizar en HTML el usuario ingresado/registrado
-    let contenedor_usuario = document.getElementById("userIcon");
-    let usuario_activo = document.createElement("p");
+    let contenedor_usuario = document.getElementById(`userIcon`);
+    let usuario_activo = document.createElement(`p`);
     usuario_activo.textContent = usuarioActivo.usuario;
     contenedor_usuario.appendChild(usuario_activo);
     usuario_activo.classList.add("active_user");
@@ -202,14 +137,13 @@ function plasmar_usuario() {
 
 // R
 function registrar() {
-
-    let usuario = document.getElementById("user").value;
-    let nombre = document.getElementById("name").value.toLowerCase();
-    let apellido = document.getElementById("lastName").value.toLowerCase();
-    let clave = document.getElementById("password").value;
-    let edad = document.getElementById("age").value;
-    let alimentacion = document.getElementById("feeding_type").value;
-    let mail = document.getElementById("email").value;
+    let usuario = document.getElementById(`user`).value;
+    let nombre = document.getElementById(`name`).value.toLowerCase();
+    let apellido = document.getElementById(`lastName`).value.toLowerCase();
+    let clave = document.getElementById(`password`).value;
+    let edad = document.getElementById(`age`).value;
+    let alimentacion = document.getElementById(`feeding_type`).value;
+    let mail = document.getElementById(`email`).value;
     let mjs = chequear_dato(usuario, nombre, apellido, clave, edad, alimentacion, mail);
     if (mjs == ``) {
         usuarioActivo = new Clientes(usuario, nombre, apellido, clave, edad, alimentacion, mail)
--- script/user.js
+++ script/user.js
@@ -11,18 +11,19 @@ class Clientes {
 }
 
 let usuarioActivo;
-let tipo_dieta = ["carnivora", "vegetariana", "vegana", "celiaca", "mixta"];
+let tipo_dieta = [`carnivora`, `vegetariana`, `vegana`, `celiaca`, `mixta`];
 
 // INGRESAR
-let login = document.getElementById("login");
-login.addEventListener("click", ingresar);
-let clave_input = document.getElementById("password");
-clave_input.addEventListener("keydown", enter);
+let login = document.getElementById(`login`);
+login.addEventListener(`click`, ingresar);
+let clave_input = document.getElementById(`password`);
+clave_input.addEventListener(`keydown`, enter);
+
 
 // REGISTRAR
-let register = document.getElementById("register");
-register.addEventListener("mousedown", habilitar_inputs);
-register.addEventListener("mousedown", inhabilitar_ingreso);
-register.addEventListener("click", registrar);
-let mail_input = document.getElementById("email");
-mail_input.addEventListener("keydown", enter);
+let register = document.getElementById(`register`);
+register.addEventListener(`mousedown`, habilitar_inputs);
+register.addEventListener(`mousedown`, inhabilitar_ingreso);
+register.addEventListener(`click`, registrar);
+let mail_input = document.getElementById(`email`);
+mail_input.addEventListener(`keydown`, enter);
